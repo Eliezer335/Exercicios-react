51790744bb2e12c2991310892ade079ade04e8e3
Eliezer335 <jimmyeliezerdasilva@gmail.com>
2025-05-13 20:36:27 -0300

    ♻️ Refatorando:  usando apenas o componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import './/componentes/contador'
+import Contador from './componentes/Contador'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [tema, setTema] = useState(true)
-
+  
   return (
     <>
-      <div className='contador'>
-        {count}
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Somar 1
-        </button>
-        <button onClick={() => setCount(0)}>
-          Reset
-        </button>
-        <button onClick={() => setTema(prev => !prev)}>
-          Mudar Tema
-        </button>
-      </div>
+      <Contador/>   
     </>
   )
 }
