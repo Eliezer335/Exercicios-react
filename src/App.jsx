764109df0012d89e215a64401cc28a91ec6c5d6c
Eliezer335 <jimmyeliezerdasilva@gmail.com>
2025-05-20 21:41:10 -0300

    ✨ Feat:  Passando os valores das props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Contador from './componentes/Contador'
+import ExercicioProps from './componentes/ExercicioProps'
 
 function App() {
   
   return (
     <>
-      <Contador/>   
+      {/* <Contador/>    */}
+      <ExercicioProps nome="Maria Souza" idade="28"  profissao="Engenheira de Software"/>
+      <ExercicioProps nome="Eliezer Martinhago" idade="25"  profissao="Engenheiro de Software"/>
     </>
   )
 }
